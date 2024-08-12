2413de19566a0cf017114c0f65937871008453db
jigong2024 <choijming21@naver.com>
2024-08-12 14:32:25 +0900

    feat: 투두리스트 화면에 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TodoList from "./components/TodoList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <TodoList />
+    </main>
+  );
 };
 
 export default App;
