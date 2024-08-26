55ae1f526b61ba2d855ba5081c27c92d52b23a4b
jigong2024 <choijming21@naver.com>
2024-08-26 14:38:30 +0900

    layout 사용

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import TodoContainer from "./components/TodoContainer";
 import "./App.css";
 import SampleTodoContext from "./contexts/SampleTodoContext";
 import SAMPLE_TODOS from "./contexts/SampleTodo";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
-    <SampleTodoContext.Provider value={SAMPLE_TODOS}>
-      <main className="main-container">
-        <TodoContainer />
-      </main>
-    </SampleTodoContext.Provider>
+    <Layout>
+      <SampleTodoContext.Provider value={SAMPLE_TODOS}>
+        <main className="main-container">
+          <TodoContainer />
+        </main>
+      </SampleTodoContext.Provider>
+    </Layout>
   );
 };
 
