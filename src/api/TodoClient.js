28f75f5fed063ce51a9d9a6b6c2affe28ab569c7
jigong2024 <choijming21@naver.com>
2024-09-11 19:38:20 +0900

    api 세팅 및 투두컨텍스트 날려버리기

--- src/api/TodoClient.js
+++ src/api/TodoClient.js
@@ -1,7 +1,37 @@
 import axios from "axios";
 
-const todoClient = axios.create({
+export const todoClient = axios.create({
   baseURL: "http://localhost:3000/todos",
 });
 
-export default todoClient;
+export const getTodos = async () => {
+  const { data } = await todoClient.get("/");
+
+  return data;
+};
+
+export const getTodoDetail = async (id) => {
+  const { data } = await todoClient.get(`/${id}`);
+
+  return data;
+};
+
+export const postTodo = async (newTodo) => {
+  const { data } = await todoClient.post("/", newTodo);
+
+  return data;
+};
+
+export const toggleTodo = async ({ id, completed }) => {
+  const { data } = await todoClient.patch(`/${id}`, {
+    completed,
+  });
+
+  return data;
+};
+
+export const deleteTodo = async (id) => {
+  const { data } = await todoClient.delete(`/${id}`);
+
+  return data;
+};
