f0d0e942bed9a494507788c510cc7a3a64388371
jigong2024 <choijming21@naver.com>
2024-09-11 19:26:27 +0900

    쿼리클라이언트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import TodoRoutes from "./router/TodoRoutes";
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <TodoRoutes />;
+  return;
+
+  <QueryClientProvider client={queryClient}>
+    <TodoRoutes />;
+  </QueryClientProvider>;
 };
 
 export default App;
